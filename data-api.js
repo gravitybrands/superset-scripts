ad979a383240685a8c5fe76fe537d38f374f4d6d
gravitybrands <derek@gravitybrands.com>
2017-04-21 15:18:04 -0700

    Update data-api.js

--- data-api.js
+++ data-api.js
@@ -1,13 +1,42 @@
-data = (function(){
+
+  
+  var data = (function(){
   
   function DataApi(){}
- 
-  DataApi.prototype.load = function(options, cb) {
-    console.log('loading data');
-  }
-  
-  return new DataApi();
-  
-})();
-console.log(data);
 
+    DataApi.prototype.load = function(opts, cb) {
+      
+      var endpoint = "http://ec2-54-234-149-129.compute-1.amazonaws.com:8088/superset/explore_json/table/"+opts.table+"/?csv=true&form_data="
+      
+      var options = {
+        row_limit: null,
+        page_length: 0,
+        include_search: false,
+        table_filter: false
+      };
+      
+      options.datasource = opts.table + "__table"
+      options.viz_type='table';
+      options.granularity_sqla = (opts.granularity_sqla || 'report_ts');
+      options.time_grain_sqla = (opts.time_grain_sqla || "Time Column");
+      options.since = opts.since || '100 years ago';
+      options.until = opts.until || 'now';
+      options.groupBy = opts.groups || [];
+      options.metrics = opts.metrics || [];
+      options.include_time = opts.include_time || true;
+      options.order_by_cols = opts.order_by_cols || [];
+      options.where = opts.where || "";
+      options.having = opts.having || "";
+      options.filters = opts.filters || "";
+      options.table_time_stamp_format = opts.timestamp_format || '%Y-%m-%d %H:%M:%S'
+      
+      endpoint += encodeURIComponent(JSON.stringify(options));
+      d3.csv(endpoint, function(data) {
+      	cb(data);
+      });
+    }
+
+    return new DataApi();
+
+  })();
+  
